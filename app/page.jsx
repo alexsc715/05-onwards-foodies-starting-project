013f0ee094b4c0d96cb0544d300969d346277ff7
alexsc715 <alexscjava@gmail.com>
2025-11-14 16:15:33 -0300

    Slideshow

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,31 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
-    <main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food fro NextLevel Foodies</h1>
+            <p>Taste & Share food from all over the world</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main></main>
+    </>
+  );
+}
+/*
+<main>
       <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
@@ -16,5 +39,4 @@ export default function Home() {
         <Link href="/community">Meals</Link>
       </p>
     </main>
-  );
-}
+*/
